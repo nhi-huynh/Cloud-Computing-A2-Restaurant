3708d40ecd3fe6f4c5af4bf62d0376fefbb907d1
Nhi Huynh <uyennhi.huynhluu@gmail.com>
2019-09-25 10:48:43 +1000

    Add Google Pay functionality

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 // env_variables:
 //   BUCKET_NAME: "example-gcs-bucket"
 
+
+
 // Store user details
 var user_id, user_name;
-var user_email = "uyennhi.huynhluu@gmail.com";  //hard-coded to test features
+var user_email = "uyennhi.huynhluu@gmail.com";  //hard-coded to test features. Supposed to be read from the Google profile.
 
+// Authentication using Google OAuth
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
     user_id = profile.getId();
@@ -12,13 +15,12 @@ function onSignIn(googleUser) {
     user_email = profile.getEmail();
 
     document.getElementById("requestLogin").innerHTML = "Hi " + user_name + "! What would you like to eat today?";
-    document.getElementById("requestLogin").style.display = "block";
-    document.getElementById("menu").style.display = "block";
     
+    // Show all dishes in the menu
     fetchDishes();
-    
   }
-  
+
+// Sign out using Google OAuth 
 function signOut() {
   var auth2 = gapi.auth2.getAuthInstance();
   auth2.signOut().then(function () {
@@ -28,8 +30,9 @@ function signOut() {
   document.getElementById("name").style.display = "none";
   document.getElementById("menu").style.display = "none";
   document.getElementById("requestLogin").innerHTML = "Login to order some yummy food!";
-    
 }
+
+
 
 // Access to AWS DynamoDB 
 AWS.config.update({
@@ -39,12 +42,12 @@ AWS.config.update({
   secretAccessKey: "rJ/brNxb6MF+/p1JEO8Ny1KmGhAFj5wW3SIi7zUI"
 });
 
-
 var docClient = new AWS.DynamoDB.DocumentClient();
 var s3 = new AWS.S3();
 
-
+// Show all dishes in the menu
 function fetchDishes(){
+  localStorage.clear();
 
   var params = {
     TableName: 'foodInformation'
@@ -60,17 +63,20 @@ function fetchDishes(){
       var fooddata = data;
       console.log (fooddata)
       
+      var dishesList = document.getElementById('dishesList');
+      dishesList.innerHTML = '<h2>Menu</h2><p><br/></p>';
+
       for (var key in data) {
           if (data.hasOwnProperty(key)) {
           var obj = data[key];
           for (var prop in obj) {
-            document.getElementById('dishesList').innerHTML += ('<div class="container"><img src='+obj[prop].imageurl+' alt="food image" style="width:170px;height:160px;border:0;"><h5><strong>'
+            dishesList.innerHTML += ('<img src='+obj[prop].imageurl+' alt="food image" style="width:170px;height:160px;border:0;"><h5><strong>'
                               +obj[prop].name+'</strong></h5><p><i>'
                               +obj[prop].description+'</i></p><b>$'
                               +obj[prop].cost+'</b><p>'
                               +obj[prop].location+'</p>'+
-                              '<button id = "addToCartButton" dish_data=' +JSON.stringify(obj[prop]).replace(/ /g,"_")+ ' onclick="addDishToCart(this); " class="btn btn-primary">Add to Cart</button><hr></div>');
-          }
+                              '<button id = "addToCartButton" dish_data=' +JSON.stringify(obj[prop]).replace(/ /g,"_")+ ' onclick="addToCart(this); " class="btn btn-primary">Add to Cart</button><hr>');
+                            }
         }
       }
     }
@@ -79,88 +85,41 @@ function fetchDishes(){
   }
 }
 
-// document.getElementById('addButton').addEventListener('click', addCart);
 
-// function addCart(e) {
-//   var dishDesc = document.getElementById('dishURL').value;
-//   var disheseverity = document.getElementById('disheseverityInput').value;
-//   var dishAssignedTo = document.getElementById('dishAssignedToInput').value;
-//   var dishId = chance.guid();
-//   var dishestatus = 'Unselectect';
 
-//   var dish = {
-//     id: dishId,
-//     description: dishDesc,
-//     severity: disheseverity,
-//     assignedTo: dishAssignedTo,
-//     status: dishestatus
-//   }
-
-//   if (localStorage.getItem('dishes') == null) {
-//     var dishes = [];
-//     dishes.push(dish);
-//     localStorage.setItem('dishes', JSON.stringify(dishes));
-//   } else {
-//     var dishes = JSON.parse(localStorage.getItem('dishes'));
-//     dishes.push(dish);
-//     localStorage.setItem('dishes', JSON.stringify(dishes));
-//   }
-
-//   document.getElementById('dishInputForm').reset();
-
-//   fetchdishes();
-
-//   e.preventDefault();
-// }
-// Add the dish to cart, stored in localStorage
-document.getElementById('addToCartButton').addEventListener('click', addDishToCart);
-function addDishToCart(event){
-  //If cart_data is empty right now
+// Add a dish to cart when user click add to cart button
+function addToCart(event){
+  var dish_data = JSON.parse(event.getAttribute("dish_data").replace(/_/g," "));
   var cart_data = {};
+
+  //If cart_data is empty right now
   if (localStorage.getItem('cart_data') == null) {
     cart_data["time"] = Date.now();
     cart_data["user_email"] = user_email;
     cart_data["dishes"] = [];
+    cart_data["total_cost"] = 0;
   } 
   else {
-    // localStorage.clear();
     cart_data = JSON.parse(localStorage.getItem('cart_data'));
     if (!cart_data.hasOwnProperty("dishes")){
       cart_data["dishes"] = [];
     }
   }
 
-  var dish_data = JSON.parse(event.getAttribute("dish_data"));
   cart_data["dishes"].push(dish_data); 
+  cart_data["total_cost"] += dish_data.cost;
 
-  
   localStorage.setItem('cart_data', JSON.stringify(cart_data));
 
   console.log("Add new item to cart_data");
   console.log(cart_data);
-  //document.getElementById('dishInputForm').reset();
 
+  // Show items in cart
   fetchCart();
-
-  // e.preventDefault();
-
-  // dish_data = dish.getAttribute("cart_data");
-  // dish_data = dish_data.replace(/_/g," ")
-  // dish_data = JSON.parse(dish_data)
-
-  // console.log(dish_data)
-
-  // var currentCart = JSON.parse( localStorage.getItem('cart') ) || {};
   
-  // currentCart["dishes"].push(dish_data);
-  // localStorage.setItem('cart', JSON.stringify(selectedMenu));
-  
-  // var de = JSON.parse(window.localStorage.getItem("cart"));
-  // console.log(de);
-  
+  // Redirect the user to the cart page
   // var url= "/cart"; 
   // window.location = url; 
-  
 }
 
 // Load dishes from cart (stored in localStorage)
@@ -175,27 +134,80 @@ function fetchCart() {
       console.log(cart_data);
 
       var cartList = document.getElementById('cartList');
+      
 
-      cartList.innerHTML = '';
+      if(ordered_dishes.length != 0){
+        cartList.innerHTML = '<h2>Your cart</h2><p><br/></p>';
+      }
+      else{
+        cartList.innerHTML = "";
+      }
 
       for (var i = 0; i < ordered_dishes.length; i++) {
         var imageURL = ordered_dishes[i].imageurl;
         var dish_name = ordered_dishes[i].name;
+        //var description = ordered_dishes[i].description;
         var cost = ordered_dishes[i].cost;
         var location = ordered_dishes[i].location;
 
-        dishesList.innerHTML +=   ('<div class="container"><img src=' + imageURL +' alt="food image" style="width:170px;height:160px;border:0;"><h5><strong>'
-                                +dish_name+'</strong></h5><p><i>'
-                                // +obj[prop].description+'</i></p><b>$'
-                                +cost+'</b><p>'
-                                +location+'</p>'+
-                                '<button id = "removeFromCartButton" dish_data=' +JSON.stringify(ordered_dishes[i]).replace(/ /g,"_")+ ' onclick="removeFromCart(this); " class="btn btn-danger">Remove from cart</button><hr></div>');
+        cartList.innerHTML +=   ('<img src=' + imageURL +' alt="food image" style="width:170px;height:160px;border:0;"><h5><strong>'
+                                + dish_name + '</strong></h5><b>$'
+                                + cost + '</b><p>'
+                                + location+'</p>'+
+                                '<button id = "removeFromCartButton" dish_data=' +JSON.stringify(ordered_dishes[i]).replace(/ /g,"_")+ ' onclick="removeFromCart(this); " class="btn btn-danger">Remove from cart</button><hr>');
+      } 
+
+      if(ordered_dishes.length != 0){
+        cartList.innerHTML += '<h3>Total cost: <b>$' + cart_data["total_cost"].toFixed(2) + ' </b></h3>';
+        if (window.location.href.indexOf('/')){
+          cartList.innerHTML += '<a href="payment.html"  class="btn btn-success" style="float: right;">Pay now</a><p><br/></p>';
+        }
+        
       }
     }
-    
+  }
+}
+
+
+
+// Add a dish to cart when user click add to cart button
+function removeFromCart(event){
+
+  // Reading from the JSON passed by the removeFromCart button
+  var to_remove_dish = JSON.parse(event.getAttribute("dish_data").replace(/_/g," "));
+  var to_remove_dish_name = to_remove_dish.name;
+  var to_remove_cost = to_remove_dish.cost;
+
+  // Get object cart_data from localStorage
+  var cart_data = JSON.parse(localStorage.getItem('cart_data'));
+  var ordered_dishes = cart_data["dishes"];
+
+  // Loop through ordered_dishes to find the dish that matches dish_name
+  for (var i = 0; i < ordered_dishes.length; i++) {
+    var dish_name = ordered_dishes[i].name;
+
+    if (dish_name.localeCompare(to_remove_dish_name) == 0){
+      ordered_dishes.splice(i, 1);
+      cart_data["total_cost"] -= to_remove_cost;
+      break;
+    }
   }
   
+  // Update cart_data in localStorage
+  cart_data["dishes"] = ordered_dishes; 
+  localStorage.setItem('cart_data', JSON.stringify(cart_data));
+
+  console.log("Removed an item from cart_data");
+  console.log(cart_data);
+
+  // Show items in cart
+  fetchCart();
+  
+  // Redirect the user to the cart page
+  // var url= "/cart"; 
+  // window.location = url; 
 }
+
 
 // document.addEventListener("DOMContentLoaded", function(event) { 
 // 	var data = JSON.parse(localStorage.getItem("food"));
@@ -207,77 +219,6 @@ function fetchCart() {
 // 																						+data[key].cost+'</h3><p>'
 // 																						+data[key].location+'</p>'+
 // 																						'<button id = "CancelButton" obj_data=' +JSON.stringify(data[key]).replace(/ /g,"_")+ ' onclick="removeFunction(this);">Remove</button>');
-							 
 // 						  }
 // 					   }
 // 	});
-	
-	
-	function removeFunction(obj){
-			data = obj.getAttribute("obj_data");
-
-			data = data.replace(/_/g," ")
-
-			data = JSON.parse(data)
-			console.log(data);
-			console.log(data.name);
-			//data = obj.getAttribute("imageurl": obj.imageurl);
-			//window.localStorage.removeItem(data.name);
-	}
-
-
-// dishes app source code
-
-
-function setStatusClosed(id) {
-  var dishes = JSON.parse(localStorage.getItem('dishes'));
-
-  for (var i = 0; i < dishes.length; i++) {
-    if (dishes[i].id == id) {
-      dishes[i].status = 'Closed';
-    }
-  }
-
-  localStorage.setItem('dishes', JSON.stringify(dishes));
-
-  fetchdishes();
-}
-
-function deletedish(id) {
-  var dishes = JSON.parse(localStorage.getItem('dishes'));
-
-  for (var i = 0; i < dishes.length; i++) {
-    if (dishes[i].id == id) {
-      dishes.splice(i, 1);
-    }
-  }
-
-  localStorage.setItem('dishes', JSON.stringify(dishes));
-
-  fetchdishes();
-}
-
-function fetchdishes() {
-  var dishes = JSON.parse(localStorage.getItem('dishes'));
-  var dishesListe = document.getElementById('dishesList');
-
-  dishesList.innerHTML = '';
-
-  for (var i = 0; i < dishes.length; i++) {
-    var id = dishes[i].id;
-    var desc = dishes[i].description;
-    var severity = dishes[i].severity;
-    var assignedTo = dishes[i].assignedTo;
-    var status = dishes[i].status;
-
-    dishesList.innerHTML +=   '<div class="well">'+
-                              '<h6>dish ID: ' + id + '</h6>'+
-                              '<p><span class="label label-info">' + status + '</span></p>'+
-                              '<h3>' + desc + '</h3>'+
-                              '<p><span class="glyphicon glyphicon-time"></span> ' + severity + '</p>'+
-                              '<p><span class="glyphicon glyphicon-user"></span> ' + assignedTo + '</p>'+
-                              '<a href="#" onclick="setStatusClosed(\''+id+'\')" class="btn btn-warning">Close</a> '+
-                              '<a href="#" onclick="deletedish(\''+id+'\')" class="btn btn-danger">Delete</a>'+
-                              '</div>';
-  }
-}
--- payment.js
+++ payment.js
@@ -1,87 +1,243 @@
-// Issues app source code
-document.getElementById('issueInputForm').addEventListener('submit', saveIssue);
+// This code is taken from https://developers.google.com/pay/api/web/guides/tutorial
 
-function saveIssue(e) {
-  var issueDesc = document.getElementById('issueDescInput').value;
-  var issueSeverity = document.getElementById('issueSeverityInput').value;
-  var issueAssignedTo = document.getElementById('issueAssignedToInput').value;
-  var issueId = chance.guid();
-  var issueStatus = 'Open';
+/**
+ * Define the version of the Google Pay API referenced when creating your
+ * configuration
+ *
+ * @see {@link https://developers.google.com/pay/api/web/reference/object#PaymentDataRequest|apiVersion in PaymentDataRequest}
+ */
+const baseRequest = {
+  apiVersion: 2,
+  apiVersionMinor: 0
+};
 
-  var issue = {
-    id: issueId,
-    description: issueDesc,
-    severity: issueSeverity,
-    assignedTo: issueAssignedTo,
-    status: issueStatus
+/**
+ * Card networks supported by your site and your gateway
+ *
+ * @see {@link https://developers.google.com/pay/api/web/reference/object#CardParameters|CardParameters}
+ * @todo confirm card networks supported by your site and gateway
+ */
+const allowedCardNetworks = ["AMEX", "DISCOVER", "INTERAC", "JCB", "MASTERCARD", "VISA"];
+
+/**
+ * Card authentication methods supported by your site and your gateway
+ *
+ * @see {@link https://developers.google.com/pay/api/web/reference/object#CardParameters|CardParameters}
+ * @todo confirm your processor supports Android device tokens for your
+ * supported card networks
+ */
+const allowedCardAuthMethods = ["PAN_ONLY", "CRYPTOGRAM_3DS"];
+
+/**
+ * Identify your gateway and your site's gateway merchant identifier
+ *
+ * The Google Pay API response will return an encrypted payment method capable
+ * of being charged by a supported gateway after payer authorization
+ *
+ * @todo check with your gateway on the parameters to pass
+ * @see {@link https://developers.google.com/pay/api/web/reference/object#Gateway|PaymentMethodTokenizationSpecification}
+ */
+const tokenizationSpecification = {
+  type: 'PAYMENT_GATEWAY',
+  parameters: {
+    'gateway': 'example',
+    'gatewayMerchantId': 'exampleGatewayMerchantId'
   }
+};
 
-  if (localStorage.getItem('issues') == null) {
-    var issues = [];
-    issues.push(issue);
-    localStorage.setItem('issues', JSON.stringify(issues));
-  } else {
-    var issues = JSON.parse(localStorage.getItem('issues'));
-    issues.push(issue);
-    localStorage.setItem('issues', JSON.stringify(issues));
+/**
+ * Describe your site's support for the CARD payment method and its required
+ * fields
+ *
+ * @see {@link https://developers.google.com/pay/api/web/reference/object#CardParameters|CardParameters}
+ */
+const baseCardPaymentMethod = {
+  type: 'CARD',
+  parameters: {
+    allowedAuthMethods: allowedCardAuthMethods,
+    allowedCardNetworks: allowedCardNetworks
   }
+};
 
-  document.getElementById('issueInputForm').reset();
+/**
+ * Describe your site's support for the CARD payment method including optional
+ * fields
+ *
+ * @see {@link https://developers.google.com/pay/api/web/reference/object#CardParameters|CardParameters}
+ */
+const cardPaymentMethod = Object.assign(
+  {},
+  baseCardPaymentMethod,
+  {
+    tokenizationSpecification: tokenizationSpecification
+  }
+);
 
-  fetchIssues();
+/**
+ * An initialized google.payments.api.PaymentsClient object or null if not yet set
+ *
+ * @see {@link getGooglePaymentsClient}
+ */
+let paymentsClient = null;
 
-  e.preventDefault();
+/**
+ * Configure your site's support for payment methods supported by the Google Pay
+ * API.
+ *
+ * Each member of allowedPaymentMethods should contain only the required fields,
+ * allowing reuse of this base request when determining a viewer's ability
+ * to pay and later requesting a supported payment method
+ *
+ * @returns {object} Google Pay API version, payment methods supported by the site
+ */
+function getGoogleIsReadyToPayRequest() {
+  return Object.assign(
+      {},
+      baseRequest,
+      {
+        allowedPaymentMethods: [baseCardPaymentMethod]
+      }
+  );
 }
 
-function setStatusClosed(id) {
-  var issues = JSON.parse(localStorage.getItem('issues'));
-
-  for (var i = 0; i < issues.length; i++) {
-    if (issues[i].id == id) {
-      issues[i].status = 'Closed';
-    }
-  }
-
-  localStorage.setItem('issues', JSON.stringify(issues));
-
-  fetchIssues();
+/**
+ * Configure support for the Google Pay API
+ *
+ * @see {@link https://developers.google.com/pay/api/web/reference/object#PaymentDataRequest|PaymentDataRequest}
+ * @returns {object} PaymentDataRequest fields
+ */
+function getGooglePaymentDataRequest() {
+  const paymentDataRequest = Object.assign({}, baseRequest);
+  paymentDataRequest.allowedPaymentMethods = [cardPaymentMethod];
+  paymentDataRequest.transactionInfo = getGoogleTransactionInfo();
+  paymentDataRequest.merchantInfo = {
+    // @todo a merchant ID is available for a production environment after approval by Google
+    // See {@link https://developers.google.com/pay/api/web/guides/test-and-deploy/integration-checklist|Integration checklist}
+    // merchantId: '01234567890123456789',
+    merchantName: 'Example Merchant'
+  };
+  return paymentDataRequest;
 }
 
-function deleteIssue(id) {
-  var issues = JSON.parse(localStorage.getItem('issues'));
-
-  for (var i = 0; i < issues.length; i++) {
-    if (issues[i].id == id) {
-      issues.splice(i, 1);
-    }
+/**
+ * Return an active PaymentsClient or initialize
+ *
+ * @see {@link https://developers.google.com/pay/api/web/reference/client#PaymentsClient|PaymentsClient constructor}
+ * @returns {google.payments.api.PaymentsClient} Google Pay API client
+ */
+function getGooglePaymentsClient() {
+  if ( paymentsClient === null ) {
+    paymentsClient = new google.payments.api.PaymentsClient({environment: 'TEST'});
   }
-
-  localStorage.setItem('issues', JSON.stringify(issues));
-
-  fetchIssues();
+  return paymentsClient;
 }
 
-function fetchIssues() {
-  var issues = JSON.parse(localStorage.getItem('issues'));
-  var issuesListe = document.getElementById('issuesList');
+/**
+ * Initialize Google PaymentsClient after Google-hosted JavaScript has loaded
+ *
+ * Display a Google Pay payment button after confirmation of the viewer's
+ * ability to pay.
+ */
+function onGooglePayLoaded() {
+  const paymentsClient = getGooglePaymentsClient();
+  paymentsClient.isReadyToPay(getGoogleIsReadyToPayRequest())
+      .then(function(response) {
+        if (response.result) {
+          addGooglePayButton();
+          // @todo prefetch payment data to improve performance after confirming site functionality
+          // prefetchGooglePaymentData();
+        }
+      })
+      .catch(function(err) {
+        // show error in developer console for debugging
+        console.error(err);
+      });
+}
 
-  issuesList.innerHTML = '';
+/**
+ * Add a Google Pay purchase button alongside an existing checkout button
+ *
+ * @see {@link https://developers.google.com/pay/api/web/reference/object#ButtonOptions|Button options}
+ * @see {@link https://developers.google.com/pay/api/web/guides/brand-guidelines|Google Pay brand guidelines}
+ */
+function addGooglePayButton() {
+  const paymentsClient = getGooglePaymentsClient();
+  const button =
+      paymentsClient.createButton({onClick: onGooglePaymentButtonClicked});
+  document.getElementById('paymentForm').appendChild(button);
+}
 
-  for (var i = 0; i < issues.length; i++) {
-    var id = issues[i].id;
-    var desc = issues[i].description;
-    var severity = issues[i].severity;
-    var assignedTo = issues[i].assignedTo;
-    var status = issues[i].status;
+/**
+ * Provide Google Pay API with a payment amount, currency, and amount status
+ *
+ * @see {@link https://developers.google.com/pay/api/web/reference/object#TransactionInfo|TransactionInfo}
+ * @returns {object} transaction info, suitable for use as transactionInfo property of PaymentDataRequest
+ */
+function getGoogleTransactionInfo() {
+  return {
+    countryCode: 'AU',
+    currencyCode: 'AUD',
+    totalPriceStatus: 'FINAL',
+    // set to cart total
+    totalPrice: getTotalPrice().toString()
+  };
+}
 
-    issuesList.innerHTML +=   '<div class="well">'+
-                              '<h6>Issue ID: ' + id + '</h6>'+
-                              '<p><span class="label label-info">' + status + '</span></p>'+
-                              '<h3>' + desc + '</h3>'+
-                              '<p><span class="glyphicon glyphicon-time"></span> ' + severity + '</p>'+
-                              '<p><span class="glyphicon glyphicon-user"></span> ' + assignedTo + '</p>'+
-                              '<a href="#" onclick="setStatusClosed(\''+id+'\')" class="btn btn-warning">Close</a> '+
-                              '<a href="#" onclick="deleteIssue(\''+id+'\')" class="btn btn-danger">Delete</a>'+
-                              '</div>';
+/**
+ * Prefetch payment data to improve performance
+ *
+ * @see {@link https://developers.google.com/pay/api/web/reference/client#prefetchPaymentData|prefetchPaymentData()}
+ */
+function prefetchGooglePaymentData() {
+  const paymentDataRequest = getGooglePaymentDataRequest();
+  // transactionInfo must be set but does not affect cache
+  paymentDataRequest.transactionInfo = {
+    totalPriceStatus: 'NOT_CURRENTLY_KNOWN',
+    currencyCode: 'USD'
+  };
+  const paymentsClient = getGooglePaymentsClient();
+  paymentsClient.prefetchPaymentData(paymentDataRequest);
+}
+
+/**
+ * Show Google Pay payment sheet when Google Pay payment button is clicked
+ */
+function onGooglePaymentButtonClicked() {
+  const paymentDataRequest = getGooglePaymentDataRequest();
+  paymentDataRequest.transactionInfo = getGoogleTransactionInfo();
+
+  const paymentsClient = getGooglePaymentsClient();
+  paymentsClient.loadPaymentData(paymentDataRequest)
+      .then(function(paymentData) {
+        // handle the response
+        processPayment(paymentData);
+      })
+      .catch(function(err) {
+        // show error in developer console for debugging
+        console.error(err);
+      });
+}
+
+/**
+ * Process payment data returned by the Google Pay API
+ *
+ * @param {object} paymentData response from Google Pay API after user approves payment
+ * @see {@link https://developers.google.com/pay/api/web/reference/object#PaymentData|PaymentData object reference}
+ */
+function processPayment(paymentData) {
+  // show returned data in developer console for debugging
+    console.log(paymentData);
+  // @todo pass payment token to your gateway to process payment
+  paymentToken = paymentData.paymentMethodData.tokenizationData.token;
+  // Redirect the user to the maps page
+  var url= "/maps.html"; 
+  window.location = url; 
+}
+
+function getTotalPrice(){
+  if (localStorage.getItem('cart_data') != null){
+    var cart_data = JSON.parse(localStorage.getItem('cart_data'));
+    return cart_data["total_cost"].toFixed(2);
   }
 }
+
